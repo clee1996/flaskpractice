5477c61a76604f95d916a7109db77d0a2e44a76e
Christopher Lee <clee@keplergrp.com>
2021-07-29 16:01:15 -0400

    added create update blog posts

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,10 +30,10 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+
+
+
+
 
     return app
 
